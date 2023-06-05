6fd01cfc6f829192ec43df2a9d8c48a6a608ee1a
Gopstol <42776756+Gopstol@users.noreply.github.com>
2023-06-05 21:30:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 class Encrypt:
     def __init__(self, w, R, key, strip_extra_nulls=False):
-        self.w = w  # block size (32, 64 or 128 bits)
-        self.R = R  # number of rounds (0 to 255)
-        self.key = key  # key (0 to 2040 bits)
+        self.w = w  # размер блока (16, 32, 64)
+        self.R = R  # число этапов (0 - 255)
+        self.key = key  # значение ключа
         self.strip_extra_nulls = strip_extra_nulls
-        # some useful constants
+
         self.T = 2 * (R + 1)
         self.w4 = w // 4
         self.w8 = w // 8
         self.mod = 2 ** self.w
         self.mask = self.mod - 1
-        self.b = len(key)
+        self.b = len(key)  # длина ключа
+
+        self.counter_int = 0
+        self.counter = self.counter_int.to_bytes(w//2, byteorder="little")
+        self.nonce = (5).to_bytes(w//2, byteorder="little")
+        self.iv = self.counter + self.nonce
+
+        self.gamma_block = b''
 
         self.__keyAlign()
         self.__keyExtend()
-        self.__shuffle()
+        self.shuffle()
 
-    def __lshift(self, val, n):
+    def lshift(self, val, n):
         n %= self.w
         return ((val << n) & self.mask) | ((val & self.mask) >> (self.w - n))
 
-    def __rshift(self, val, n):
+    def rshift(self, val, n):
         n %= self.w
         return ((val & self.mask) >> n) | (val << (self.w - n) & self.mask)
 
-    def __const(self):  # constants generation
+    def const(self):
         if self.w == 16:
-            return 0xB7E1, 0x9E37  # return P (e), Q (pi) values
+            return 0xB7E1, 0x9E37  # return P (e), Q (pi)
         elif self.w == 32:
             return 0xB7E15163, 0x9E3779B9
         elif self.w == 64:
             return 0xB7E151628AED2A6B, 0x9E3779B97F4A7C15
 
     def __keyAlign(self):
-        if self.b == 0:  # key is empty
+        if self.b == 0:
             self.c = 1
         elif self.b % self.w8:
-            self.key += b'\x00' * (self.w8 - self.b % self.w8)  # fill key with \x00 bytes
+            self.key += b'\x00' * (self.w8 - self.b % self.w8)  # заполняет ключ \x00 битами если нужно
             self.b = len(self.key)
             self.c = self.b // self.w8
         else:
@@ -53,34 +54,34 @@ class Encrypt:
         self.L = L
 
     def __keyExtend(self):
-        P, Q = self.__const()
+        P, Q = self.const()
         self.S = [(P + i * Q) % self.mod for i in range(self.T)]
 
-    def __shuffle(self):
+    def shuffle(self):
         i, j, A, B = 0, 0, 0, 0
         for k in range(3 * max(self.c, self.T)):
-            A = self.S[i] = self.__lshift((self.S[i] + A + B), 3)
-            B = self.L[j] = self.__lshift((self.L[j] + A + B), A + B)
+            A = self.S[i] = self.lshift((self.S[i] + A + B), 3)
+            B = self.L[j] = self.lshift((self.L[j] + A + B), A + B)
             i = (i + 1) % self.T
             j = (j + 1) % self.c
 
-    def encryptBlock(self, data):
-        A = int.from_bytes(data[:self.w8], byteorder='little')
-        B = int.from_bytes(data[self.w8:], byteorder='little')
+    def encryptBlock(self, A, B):
+        A = int.from_bytes(A, byteorder="little")
+        B = int.from_bytes(B, byteorder="little")
         A = (A + self.S[0]) % self.mod
         B = (B + self.S[1]) % self.mod
         for i in range(1, self.R + 1):
-            A = (self.__lshift((A ^ B), B) + self.S[2 * i]) % self.mod
-            B = (self.__lshift((A ^ B), A) + self.S[2 * i + 1]) % self.mod
+            A = (self.lshift((A ^ B), B) + self.S[2 * i]) % self.mod
+            B = (self.lshift((A ^ B), A) + self.S[2 * i + 1]) % self.mod
         return (A.to_bytes(self.w8, byteorder='little')
                 + B.to_bytes(self.w8, byteorder='little'))
 
-    def decryptBlock(self, data):
-        A = int.from_bytes(data[:self.w8], byteorder='little')
-        B = int.from_bytes(data[self.w8:], byteorder='little')
+    def decryptBlock(self, A, B):
+        A = int.from_bytes(A, byteorder='little')
+        B = int.from_bytes(B, byteorder='little')
         for i in range(self.R, 0, -1):
-            B = self.__rshift(B - self.S[2 * i + 1], A) ^ A
-            A = self.__rshift(A - self.S[2 * i], B) ^ B
+            B = self.rshift(B - self.S[2 * i + 1], A) ^ A
+            A = self.rshift(A - self.S[2 * i], B) ^ B
         B = (B - self.S[1]) % self.mod
         A = (A - self.S[0]) % self.mod
         return (A.to_bytes(self.w8, byteorder='little')
@@ -88,24 +89,21 @@ class Encrypt:
 
     def encryptFile(self, inpFileName, outFileName):
         with open(inpFileName, 'rb') as inp, open(outFileName, 'wb') as out:
+            self.counter_int = 0
+            self.counter = self.counter_int.to_bytes(self.w // 2, byteorder="little")
+            self.nonce = (5).to_bytes(self.w // 2, byteorder="little")
+            self.iv = self.counter + self.nonce
             run = True
             while run:
                 text = inp.read(self.w4)
                 if not text:
                     break
                 if len(text) != self.w4:
-                    text = text.ljust(self.w4, b'\x00')
                     run = False
-                text = self.encryptBlock(text)
-                out.write(text)
-
-    def decryptFile(self, inpFileName, outFileName):
-        with open(inpFileName, 'rb') as inp, open(outFileName, 'wb') as out:
-            while True:
-                text = inp.read(self.w4)
-                if not text:
-                    break
-                text = self.decryptBlock(text)
-                if self.strip_extra_nulls:
-                    text = text.rstrip(b'\x00')
-                out.write(text)
+                self.gamma_block = self.encryptBlock(self.key, self.iv)
+                text = int.from_bytes(text, byteorder="little") ^ int.from_bytes(self.gamma_block, byteorder="little")
+                self.counter_int += 1
+                self.counter = self.counter_int.to_bytes(self.w // 2, byteorder="little")
+                self.iv = self.counter + self.nonce
+                # print(text, text.to_bytes(self.w4, byteorder="little"))
+                out.write(text.to_bytes(self.w4, byteorder="little"))
